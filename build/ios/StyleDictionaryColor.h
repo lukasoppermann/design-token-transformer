bd820411328d1bcae342d0e0769cb7b87cd35f1e
Lukas Oppermann <oppermann.lukas@gmail.com>
2020-09-28 14:01:12 +0200

    base setup

---
+++ build/ios/StyleDictionaryColor.h
@@ -0,0 +1,27 @@
+
+//
+// StyleDictionaryColor.h
+//
+// Do not edit directly
+// Generated on Mon, 28 Sep 2020 12:00:56 GMT
+//
+
+#import <UIKit/UIKit.h>
+
+
+typedef NS_ENUM(NSInteger, StyleDictionaryColorName) {
+ColorsOrange,
+ColorsGreen,
+ColorsBlue,
+ColorsSecondaryGreen,
+ColorsSecondaryYellow,
+UtilitiesError,
+EffectShadowColor,
+InnerShadow0Color,
+InnerShadow1Color
+};
+
+@interface StyleDictionaryColor : NSObject
++ (NSArray *)values;
++ (UIColor *)color:(StyleDictionaryColorName)color;
+@end
---
+++ build/ios/StyleDictionarySize.m
@@ -0,0 +1,54 @@
+
+//
+// StyleDictionarySize.m
+//
+// Do not edit directly
+// Generated on Mon, 28 Sep 2020 12:00:56 GMT
+//
+
+#import "StyleDictionarySize.h"
+
+
+
+float const GridBase0GutterSize = 20;
+float const GridBase0Count = 5;
+float const GridBase0Offset = 10;
+float const GridBase1SectionSize = 8;
+float const GridBase1GutterSize = 10;
+float const GridBase1Count = 5;
+float const GridBase2SectionSize = 1;
+float const GridBase2GutterSize = 20;
+float const GridBase2Count = 5;
+float const GridBase2Offset = 1;
+float const GridBase3SectionSize = 34;
+float const GridBase3GutterSize = 20;
+float const GridBase3Count = 5;
+float const GridBase3Offset = 13;
+float const GridBase4SectionSize = 8;
+float const GridBase4GutterSize = 20;
+float const GridBase4Count = 5;
+float const GridBase4Offset = 10;
+float const GridBase5SectionSize = 8;
+float const GridWeirdGridWithCommentSectionSize = 21;
+float const GridWeirdGridWithCommentGutterSize = 13;
+float const GridWeirdGridWithCommentCount = 5;
+float const GridWeirdGridWithCommentOffset = 23;
+float const GridCenteredSectionSize = 11;
+float const GridCenteredGutterSize = 20;
+float const GridCenteredCount = 5;
+float const CountAutoSectionSize = 100;
+float const CountAutoGutterSize = 21;
+float const CountAutoOffset = 30;
+float const BodyH3FontSize = 20;
+float const BodyH3LetterSpacing = 2;
+float const BodyH3LineHeight = 160;
+float const BodyH3ParagraphIndent = 5;
+float const BodyH3ParagraphSpacing = 8;
+float const EffectShadowRadius = 4;
+float const EffectShadowSpread = 0;
+float const EffectLayerBlurRadius = 4;
+float const EffectBackgroundBlurRadius = 4;
+float const InnerShadow0Radius = 4;
+float const InnerShadow0Spread = 0;
+float const InnerShadow1Radius = 10;
+float const InnerShadow1Spread = 0;
