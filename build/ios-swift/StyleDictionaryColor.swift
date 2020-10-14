15e7ca895ecca1e5c00e032d53a0e6a0b50894c9
Lukas Oppermann <oppermann.lukas@gmail.com>
2020-10-14 13:26:49 +0200

    added github workflow to transform tokens

---
+++ build/ios-swift/StyleDictionaryColor.swift
@@ -0,0 +1,21 @@
+
+//
+// StyleDictionaryColor.swift
+//
+// Do not edit directly
+// Generated on Mon, 28 Sep 2020 12:39:30 GMT
+//
+
+import UIKit
+
+public enum StyleDictionaryColor {
+    public static let blue = rgba(0.017, 0.292, 1, 1)
+    public static let green = rgba(0.25, 1, 0.73, 1)
+    public static let orange = rgba(1, 0.5, 0.25, 1)
+    public static let secondaryGreen = rgba(0.14, 0.688, 0.195, 1)
+    public static let secondaryYellow = rgba(1, 0.72, 0, 1)
+    public static let shadowColor = rgba(0, 0, 0, 0.25)
+    public static let 0Color = rgba(0, 0, 0, 0.25)
+    public static let 1Color = rgba(0, 0, 0, 0.15)
+    public static let error = rgba(1, 0, 0, 1)
+}
---
+++ build/ios/StyleDictionaryColor.m
@@ -0,0 +1,39 @@
+
+//
+// StyleDictionaryColor.m
+//
+// Do not edit directly
+// Generated on Mon, 28 Sep 2020 12:39:30 GMT
+//
+
+#import "StyleDictionaryColor.h"
+
+
+@implementation StyleDictionaryColor
+
++ (UIColor *)color:(StyleDictionaryColorName)colorEnum{
+  return [[self values] objectAtIndex:colorEnum];
+}
+
++ (NSArray *)values {
+  static NSArray* colorArray;
+  static dispatch_once_t onceToken;
+
+  dispatch_once(&onceToken, ^{
+    colorArray = @[
+rgba(1, 0.5, 0.25, 1),
+rgba(0.25, 1, 0.73, 1),
+rgba(0.017, 0.292, 1, 1),
+rgba(0.14, 0.688, 0.195, 1),
+rgba(1, 0.72, 0, 1),
+rgba(1, 0, 0, 1),
+rgba(0, 0, 0, 0.25),
+rgba(0, 0, 0, 0.25),
+rgba(0, 0, 0, 0.15)
+    ];
+  });
+
+  return colorArray;
+}
+
+@end
