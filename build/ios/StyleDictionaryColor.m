6c4a0427d10ea0e318fcb22d67668f2fc46fa573
lukasoppermann <lukasoppermann@users.noreply.github.com>
2022-06-16 10:25:42 +0000

    Tokens updated

--- build/ios/StyleDictionaryColor.m
+++ build/ios/StyleDictionaryColor.m
@@ -4,7 +4,7 @@
 //
 
 // Do not edit directly
-// Generated on Thu, 16 Dec 2021 18:06:29 GMT
+// Generated on Thu, 16 Jun 2022 10:25:41 GMT
 
 
 #import "StyleDictionaryColor.h"
@@ -21,14 +21,14 @@
 
   dispatch_once(&onceToken, ^{
     colorArray = @[
+[UIColor colorWithRed:1.000f green:0.373f blue:0.016f alpha:0.502f],
 [UIColor colorWithRed:0.251f green:1.000f blue:0.729f alpha:1.000f],
 [UIColor colorWithRed:0.000f green:0.000f blue:0.000f alpha:0.102f],
 [UIColor colorWithRed:0.016f green:0.290f blue:1.000f alpha:1.000f],
 [UIColor colorWithRed:0.016f green:0.290f blue:1.000f alpha:1.000f],
 [UIColor colorWithRed:0.251f green:0.875f blue:0.314f alpha:1.000f],
 [UIColor colorWithRed:0.204f green:0.337f blue:0.686f alpha:1.000f],
-[UIColor colorWithRed:1.000f green:1.000f blue:1.000f alpha:1.000f],
-[UIColor colorWithRed:0.000f green:0.000f blue:0.000f alpha:1.000f]
+[UIColor colorWithRed:0.000f green:0.000f blue:0.000f alpha:0.000f]
     ];
   });
 
