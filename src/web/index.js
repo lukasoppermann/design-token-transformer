a480d9c91c9685fc1182bb4ed603cb713514070b
Robert Andrade <robert.andrade@life.io>
2022-06-28 13:56:48 -0500

    rename webShadow and better transformGroups

--- src/web/index.js
+++ src/web/index.js
@@ -1,30 +1,42 @@
 const StyleDictionary = require('style-dictionary')
+const customTransforms = [
+  'size/pixel',
+  'size/percent',
+  'web/shadow',
+  'web/radius',
+  'web/padding',
+  'web/font',
+  'web/gradient',
+  'color/hex8ToRgba'
+]
+
+const transform = {
+  'size/pixel': require('../common/sizePixel'),
+  'size/percent': require('../common/sizePercent'),
+  'web/shadow': require('./webShadow'),
+  'web/radius': require('./webRadius'),
+  'web/padding': require('./webPadding'),
+  'web/font': require('./webFont'),
+  'web/gradient': require('./webGradient'),
+  'web/grid': require('./webGrid'),
+  'color/hex8ToRgba': require('../common/colorToRgbaString')
+}
+
+const transformGroup = {
+  'custom/scss': StyleDictionary.transformGroup.scss.concat(customTransforms),
+  'custom/less': StyleDictionary.transformGroup.less.concat(customTransforms),
+  'custom/css': StyleDictionary.transformGroup.css.concat(customTransforms),
+}
+
+const format = {
+  'custom/css': require('./formatCss')
+}
+
+const action = {}
 
 module.exports = {
-  transform: {
-    'size/px': require('../common/sizePixel'),
-    'size/percent': require('../common/sizePercent'),
-    'web/shadow': require('./webShadows'),
-    'web/radius': require('./webRadius'),
-    'web/padding': require('./webPadding'),
-    'web/font': require('./webFont'),
-    'web/gradient': require('./webGradient'),
-    'color/hex8ToRgba': require('../common/colorToRgbaString')
-  },
-  transformGroup: {
-    'custom/scss': StyleDictionary.transformGroup.css.concat([
-      'size/px',
-      'size/percent',
-      'web/shadow',
-      'web/radius',
-      'web/padding',
-      'web/font',
-      'web/gradient',
-      'color/hex8ToRgba'
-    ])
-  },
-  format: {
-    'custom/css': require('./formatCss')
-  },
-  action: {}
+  transform,
+  transformGroup,
+  format,
+  action
 }
