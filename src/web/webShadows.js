978c3e9e871982b1eff6ff81340e8895aaca8d05
Robert Andrade <robert.andrade@life.io>
2022-06-28 13:06:13 -0500

    remove design token examples and update webshadows

--- src/web/webShadows.js
+++ src/web/webShadows.js
@@ -5,7 +5,7 @@ module.exports = {
   matcher: function (token) {
     return token.type === 'custom-shadow' && token.value !== 0
   },
-  transformer: function ({ value }) {
-    return `${value.shadowType === 'innerShadow' ? 'inset ' : ''}${value.offsetX}px ${value.offsetY}px ${value.radius}px ${value.spread}px ${new TinyColor.TinyColor(value.color).toRgbString()}`
+  transformer: function ({ value: {shadowType, offsetX, offsetY, radius, spread, color} }) {
+    return `${shadowType === 'innerShadow' ? 'inset ' : ''}${offsetX}px ${offsetY}px ${radius}px ${spread}px ${new TinyColor.TinyColor(color).toRgbString()}`
   }
 }
